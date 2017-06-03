d70beb2fd3b903db90530a913863c0995ea2ffa9
Pranav <pranavsriram1@gmail.com>
2017-06-03 13:46:16 -0400

    Got Home, About us, and Our Work partially set up

---
+++ app/controllers/current_pages_controller.rb
@@ -0,0 +1,9 @@
+class CurrentPagesController < ApplicationController
+	def home
+		#Rails will look at this controller, then automatically render the view
+	end 
+	def aboutus
+	end 
+	def ourwork
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #root tells the program where to go to when we arrive on the plain URL
+  #(aka chengfoundation.us)
+  root 'current_pages#home'#hello method within application controller
+  get '/aboutus' => 'current_pages#aboutus'
+  get '/ourwork' => 'current_pages#ourwork'
 end
