d8270fd89220d01c33991d91efa38a77ba83443c
Pranav <pranavsriram1@gmail.com>
2017-06-04 10:48:57 -0400

    Testing for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #root tells the program where to go to when we arrive on the plain URL
   #(aka chengfoundation.us)
-  root 'current_pages#home'#hello method within application controller
-  get '/aboutus' => 'current_pages#aboutus'
-  get '/ourwork' => 'current_pages#ourwork'
+  
+  root 'application#hello'
+
+  # root 'current_pages#home'#hello method within application controller
+  # get '/aboutus' => 'current_pages#aboutus'
+  # get '/ourwork' => 'current_pages#ourwork'
 end
